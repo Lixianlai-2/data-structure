dcdc1ff976e09df9e307732ae068cf4071a97c3e
Lixianlai-2 <16435656550@qq.com>
2022-02-07 16:36:07 +0800

    修复代码

--- minOf2/main.js
+++ minOf2/main.js
@@ -5,12 +5,12 @@
 let maxOf2 = ([a, b]) => (a > b ? a : b);
 
 // 得到三个数中最大的那个数
-let maxOF3 = ([a, b, c]) => {
+let maxOf3 = ([a, b, c]) => {
   // 注意maxOf2的参数是被结构赋值过的，这样才能获得对应的值
-  return maxOf2([a, maxOf2(b, c)]);
+  return maxOf2([a, maxOf2([b, c])]);
 };
 
 let maxOf4 = ([a, b, c, d]) => {
   // 注意maxOf3的参数是被结构赋值过的，这样才能获得对应的值
-  return maxOf2([a, maxOF3(b, c, d)]);
+  return maxOf2([a, maxOf3([b, c, d])]);
 };
